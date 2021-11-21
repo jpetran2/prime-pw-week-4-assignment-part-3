c9df4068febbc46f1e16517d7d6aed70c695d639
James Petran <jm.ptrn@gmail.com>
2021-11-21 14:27:09 -0600

    addItem, listItems prelim creation

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,3 +2,20 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+
+
+
+
+function addItem(item,basket) {
+    let length = basket.length;
+    basket.splice(length-1, 0, item); 
+        //inserts item at end of basket[]
+    return true;
+} //end addItem
+
+function listItems(basket) {
+    for (const item of basket) {
+        console.log(item);
+    }
+    return;
+}
