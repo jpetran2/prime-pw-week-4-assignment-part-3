8c64f4ec4051e4c1fa52a2a9897913fe290bbd5a
James Petran <jm.ptrn@gmail.com>
2021-11-21 14:34:47 -0600

    list & mult items test

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -11,9 +11,12 @@ function addItem(item,basket) {
 } //end addItem
 
 function listItems(basket) {
+    console.log(`The basket contains:`);
     for (const item of basket) {
         console.log(item);
     }
+    console.log(`and that's it!`);
+    console.log('');
     return;
 } //end listItems
 
@@ -25,5 +28,18 @@ function empty(basket) {
 
 let basket = [];
 console.log(`Basket is ${basket}`);
+
+//add one item test
 console.log(`Adding pears! (expect true)`, addItem('pears',basket));
 console.log(`Basket contains ${basket}`);
+//add one item works
+
+//add multiple items test
+addItem('peaches', basket);
+addItem('pecans', basket);
+console.log(`Basket contains ${basket}`);
+//add multiple items works
+
+//read basket test
+listItems(basket);
+//
